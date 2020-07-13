68f29bd3053ef4c23349d393cc331024a830e284
iofjuupasli <iofjuupasli@gmail.com>
2020-07-13 20:39:05 +0300

    fix colors; add types

---
+++ src/lib/index.d.ts
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+declare module '@iofjuupasli/react-google-flight-datepicker' {
+  export interface RangeDatePickerProps {
+    startDate?: Date;
+    endDate?: Date;
+    startDatePlaceholder?: string;
+    endDatePlaceholder?: string;
+    className?: string;
+    disabled?: boolean;
+    onChange?: (startDate: Date, endDate: Date) => void;
+    onFocus?: (input: 'Start Date' | 'End Date' | '') => void;
+    startWeekDay?: 'monday' | 'sunday';
+    minDate?: Date;
+    maxDate?: Date;
+    dateFormat?: string;
+    monthFormat?: string;
+    highlightToday?: boolean;
+    topBar: React.ReactNode;
+    DoneButton: React.ElementType<{onClick: Function}>;
+  }
+
+  export class RangeDatePicker extends React.Component<RangeDatePickerProps> { }
+
+  export interface SingleDatePickerProps {
+    startDate?: Date;
+    startDatePlaceholder?: string;
+    className?: string;
+    disabled?: boolean;
+    onChange?: (startDate: Date) => void;
+    onFocus?: (input: 'Start Date') => void;
+    startWeekDay?: 'monday' | 'sunday';
+    minDate?: Date;
+    maxDate?: Date;
+    dateFormat?: string;
+    monthFormat?: string;
+    highlightToday?: boolean;
+    topBar: React.ReactNode;
+    DoneButton: React.ElementType<{onClick: Function}>;
+  }
+
+  export class SingleDatePicker extends React.Component<SingleDatePickerProps> { }
+}
